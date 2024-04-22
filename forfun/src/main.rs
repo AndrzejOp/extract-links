40f2d5abbc5b6d2062f49b03dded2d08c8ea96c4
AndrzejOp <andrzejopon3@gmail.com>
2024-04-22 18:09:38 +0200

    Initial

--- forfun/src/main.rs
+++ forfun/src/main.rs
@@ -1,45 +1,33 @@
-fn pw_o_1(mut x: i32) -> i32{
-    x += 1;
-    x
+#[derive(Debug, PartialEq, PartialOrd, Clone)]
+struct Punkt3D {
+    x: f64,
+    y: f64,
+    z: f64,
 }
-fn pws_o_1(x: &mut i32) {
-    *x += 1;
-}
+//kopiowanie albo przenoszenie, copy jest niejawne, ogólnie robią to samo, copy zajmuje wiecej pamieci, bo nie mamy kontroli
 
 fn main() {
-    // let mut  a = 12;
-    // let b = pw_o_1(a);
-    // println!("{}", b == 13);
-    //
-    // pws_o_1(&mut a);
-    // println!("{}", a == 13);
-    // pws_o_1(&mut a);
-    // println!("{}", a == 14);
+    let mut p1 = Punkt3D {
+        x: 4.5,
+        z: -7.5,
+        y: 3.0,
+    };
+    println!("{:?}", p1);
+    let p2 = Punkt3D {
+        z: 47.5,
+        ..p1
+    };
+    println!("{:?}", p2);
+    p1.x = 34.12;
+    println!("{:?}", p1);
+    println!("{:?}", p1 == p2);
+    println!("{:?}", p1 < p2);
+    println!("{:?}", p1 > p2);
 
-    // let mut s1 = "Ala ma kota".to_string();
-    //
-    // s1.push_str(" i psa");
-    // s1.push('.');
-    // println!("{}", s1);
-    //
-    // println!("{:?}", s1.find('a'));
-    // println!("{:?}", s1.find('x'));
-    // println!("{:?}", s1.find("a"));
-    // println!("{:?}", s1.find("kot"));
-    //
-    // let s4 = s1.replace("kota", "szczura");
-    //
-    // let a = s1.as_bytes();
-    // println!("{:?}", a);
-
-    let s0 = "Witaj swiecie";
-
-    for c in s0.chars() {
-        println!("{}", c);
-    }
-
-    for c in s0.bytes() {
-        println!("{}", c);
-    }
-
+    let p3 = p1.clone();
+    let p4 = p1.clone();
+    println!("{:?}", p1);
+    println!("{:?}", p3);
+    println!("{:?}", p4);
+    println!("{:?}", p1 == p4);
 }
